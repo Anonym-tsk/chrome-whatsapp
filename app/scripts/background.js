c5215d20d66ce1c5c9dc3ba51455e303acc921ab
Nikolay Vasilchuk <Anonym.tsk@gmail.com>
2015-01-23 12:05:35 +0300

    Size for 1024x768 screens

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -3,8 +3,8 @@ chrome.app.runtime.onLaunched.addListener(function() {
     chrome.app.window.create('index.html', {
         id: 'whatsapp',
         innerBounds: {
-            width: 900,
-            height: 720
+            width: 884,
+            height: 700
         },
         frame: 'chrome',
         resizable: false
